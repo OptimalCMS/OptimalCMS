847e1c5808d3cd630cc4629939c6858dad0be96f
OptimalCMS <85036607+OptimalCMS@users.noreply.github.com>
2021-06-04 00:47:40 +0100

    Update theme.php

--- themes/default/theme.php
+++ themes/default/theme.php
@@ -1,11 +1,20 @@
 <!DOCTYPE html>
-<html>
+<html lang="<?= site->locale ?>">
 <head>
-<title><?= page->title ?> | <?= site->title ?></title>
-<?= siteHead() ?>
+  <meta charset="<?= site->charset ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title><?= page->title ?> | <?= site->title ?></title>
+  <?= siteHead() ?>
 </head>
 <body>
-
-<?= siteEnd() ?>
+  <footer class="">
+    <p>
+      <?= block->footer ?>
+    </p>
+    <p class="powered">
+      Proudly Powered by <a href="https://optimalcms.github.io/" target="_blank">OptimalCMS</a>
+    </p>
+  </footer>
+  <?= siteEnd() ?>
 </body>
 </html>
