4dba116a7319bf014fe17e5b4f52c0e2d2ff4bba
OptimalCMS <85036607+OptimalCMS@users.noreply.github.com>
2021-06-03 10:30:40 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,15 +7,16 @@
  */
 
 
-$file = __DIR__ . '/config/site.json';
+define( 'ABSPATH', __DIR__.'/' );
+
+$file = ABSPATH . '/system/config/site.json';
 $json = file_get_contents($file);
 $site = json_decode($json);
 
 class oCMS {
-	
 	function loadPlugins()
 	{
-		$plugins = $this->rootDir . '/plugins';
+		$plugins = ABSPATH . '/plugins';
 		foreach (glob($plugins . '/*', GLOB_ONLYDIR) as $dir) {
 			if (file_exists($dir . '/' . basename($dir) . '.php')) {
 				include $dir . '/' . basename($dir) . '.php';
@@ -25,7 +26,7 @@ class oCMS {
 	
 	function loadTheme()
 	{
-		$location = $this->rootDir . '/themes/' . $this->get('config', 'theme');
+		$location = ABSPATH . '/themes/' . $this->get('config', 'theme');
 		if (file_exists($location . '/functions.php')) {
 			require_once $location . '/functions.php';
 		}
