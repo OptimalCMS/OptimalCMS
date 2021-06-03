13c0b91280a15cb5c96c7b3edae1cf220e8c52e0
OptimalCMS <85036607+OptimalCMS@users.noreply.github.com>
2021-06-03 08:17:34 +0100

    Update system.php

--- system/system.php
+++ system/system.php
@@ -1,27 +1,25 @@
 <?php
-
 $file = __DIR__ . '/config/site.json';
 $json = file_get_contents($file);
 $site = json_decode($json);
-function loadPlugins()
+
+class oCMS {
+	
+	function loadPlugins()
 	{
 		$plugins = $this->rootDir . '/plugins';
-          	foreach (glob($plugins . '/*', GLOB_ONLYDIR) as $dir) {
+		foreach (glob($plugins . '/*', GLOB_ONLYDIR) as $dir) {
 			if (file_exists($dir . '/' . basename($dir) . '.php')) {
 				include $dir . '/' . basename($dir) . '.php';
 			}
 		}
 	}
-?>
-<!DOCTYPE html>
-<html>
-<head>
-<title><?= $site->title; ?></title>
-</head>
-<body>
-
-<?= $site->description; ?>
-
-
-</body>
-</html>
+	
+	function loadTheme()
+	{
+		$location = $this->rootDir . '/themes/' . $this->get('config', 'theme');
+		if (file_exists($location . '/functions.php')) {
+			require_once $location . '/functions.php';
+		}
+		require_once $location . '/theme.php';
+	}
