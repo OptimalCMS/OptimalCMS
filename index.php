9e84e3f0ee24eedd71fb9c8fcca79c238909a464
OptimalCMS <85036607+OptimalCMS@users.noreply.github.com>
2021-06-03 10:35:21 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @package OptimalCMS
+ * @author Shoaiyb Sysa
+ * @see https://optimalcms.github.io
+ * @license MIT
+ */
+
+
+define( 'ABSPATH', __DIR__.'/' );
+
+class oCMS {
+  function site()
+  {
+    $file = ABSPATH . '/system/config/site.json';
+    $json = file_get_contents($file);
+    $site = json_decode($json);
+  }
+  
+  function loadPlugins()
+  {
+    $plugins = ABSPATH . '/plugins';
+    foreach (glob($plugins . '/*', GLOB_ONLYDIR) as $dir) {
+      if (file_exists($dir . '/' . basename($dir) . '.php')) {
+        include $dir . '/' . basename($dir) . '.php';
+      }
+    }
+  }
+	
+  function loadTheme()
+  {
+    $location = ABSPATH . '/themes/' . $this->get('config', 'theme');
+    if (file_exists($location . '/functions.php')) {
+      require_once $location . '/functions.php';
+    }
+    require_once $location . '/theme.php';
+  }
+  
+  
