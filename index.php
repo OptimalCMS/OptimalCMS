3c42397b99724d6e9f8274719857b942dd48b428
OptimalCMS <85036607+OptimalCMS@users.noreply.github.com>
2021-06-03 14:36:46 +0100

    Update index.php

--- index.php
+++ index.php
@@ -11,34 +11,32 @@ define( 'ABSPATH', __DIR__.'/' );
 define( 'SYSPATH', ABSPATH.'system/' );
 define( 'CONFPATH', SYSPATH.'config/' );
 
-class oCMS {
-  function site()
-  {
-    $location = CONFPATH .'site.json';
-    $json = file_get_contents($file);
-    $site = json_decode($json);
-  }
-  
-  function loadPlugins()
-  {
-    $plugins = ABSPATH . '/plugins';
-    foreach (glob($plugins . '/*', GLOB_ONLYDIR) as $dir) {
-      if (file_exists($dir . '/' . basename($dir) . '.php')) {
-        include $dir . '/' . basename($dir) . '.php';
-      }
-    }
-  }
-	
-  function loadTheme()
-  {
-    $location = ABSPATH . '/themes/' . $this->get('config', 'theme');
-    if (file_exists($location . '/functions.php')) {
-      require_once $location . '/functions.php';
-    }
-    require_once $location . '/theme.php';
-  }
+function site()
+{
+	$location = CONFPATH .'site.json';
+	$json = file_get_contents($file);
+	$site = json_decode($json);
 }
-  
+
+function loadPlugins()
+{
+	$plugins = ABSPATH . '/plugins';
+	foreach (glob($plugins . '/*', GLOB_ONLYDIR) as $dir) {
+		if (file_exists($dir . '/' . basename($dir) . '.php')) {
+			include $dir . '/' . basename($dir) . '.php';
+		}
+	}
+}
+
+function loadTheme()
+{
+	$location = ABSPATH . '/themes/' . $this->get('config', 'theme');
+	if (file_exists($location . '/functions.php')) {
+		require_once $location . '/functions.php';
+	}
+	require_once $location . '/theme.php';
+}
+
 
 
 
