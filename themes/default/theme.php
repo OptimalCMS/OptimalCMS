7df73332a1f9a3a9c8b7b25655320d30a02b9318
OptimalCMS <85036607+OptimalCMS@users.noreply.github.com>
2021-06-04 01:15:24 +0100

    Update theme.php

--- themes/default/theme.php
+++ themes/default/theme.php
@@ -4,10 +4,14 @@
   <meta charset="<?= site->charset ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?= page->title ?> | <?= site->title ?></title>
+  <meta name="description" content="<?= page->description ?>">
   <?= siteHead() ?>
 </head>
 <body>
-  <footer class="">
+  <div class="main">
+    <?= page->content ?>
+  </div>
+  <footer class="footer">
     <p>
       <?= block->footer ?>
     </p>
