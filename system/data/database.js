f71f43cafd0875f86a43b37d9061b730ec8ba30c
OptimalCMS <85036607+OptimalCMS@users.noreply.github.com>
2021-07-08 20:26:19 +0100

    Update and rename database.json to database.js

--- system/data/database.js
+++ system/data/database.js
@@ -1,13 +1,13 @@
 {
-  "name": "OptimalCMS",
+  "name": "OptimalCMS Open Source Almost Headless One Page Site Generator",
   "title": "OptimalCMS",
   "description": "Simple, Powerful, Optimal & Optimised site powered by OptimalCMS",
   "charset": "utf-8",
   "locale": "en",
   "email": "admin@example.com",
-  "url": "https://optimalcms.github.io",
+  "url": "https://optimalcms.ml",
   "baseurl": "/",
   "theme": "default",
   "home": "home",
-  "footer": "&copy; copyright 2021 | $site->title"
+  "footer": "&copy; copyright 2021"
 }
