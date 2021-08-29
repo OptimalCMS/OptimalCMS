d0b5fd62ce46188ad38b088990a72c357da306d1
OptimalCMS <85036607+OptimalCMS@users.noreply.github.com>
2021-08-29 21:39:28 +0100

    Create functions.php

---
+++ site/functions.php
@@ -0,0 +1,32 @@
+<?php
+
+$OptimalCMS = new OptimalCMS();
+
+function add_action($where,$callback,$priority=50) {
+  global $OptimalCMS;
+  if(isset($OptimalCMS)) {
+    $OptimalCMS->add_action($where,$callback,$priority);
+  }
+}
+
+function remove_action($where,$callback) {
+  global $OptimalCMS;
+  if(isset($OptimalCMS)) {
+    $OptimalCMS->remove_action($where,$callback);
+  }
+}
+
+function execute_action($where,$args=array()) {
+  global $OptimalCMS;
+  if(isset($OptimalCMS)) {
+    $OptimalCMS->execute($where,$args);
+  }
+}
+
+require SITE_DIR . 'db.php';
+require THEME_DIR . $site->theme . '/index.php';
+if(file_exists(THEME_DIR . $site->theme . '/functions.php')) {
+require_once(THEME_DIR . $site->theme . '/functions.php');
+}
+
+?>
