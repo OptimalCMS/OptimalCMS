2ce17e3e1297593ab8c5e3c07717ea690cb11821
OptimalCMS <85036607+OptimalCMS@users.noreply.github.com>
2021-08-29 22:17:14 +0100

    Update site.js

--- site/site.js
+++ site/site.js
@@ -2,8 +2,8 @@
   "lang": "en",
   "charset": "utf-8",
   "title": "OptimalCMS",
-  "description": "Open Source CMS to build optimized onepage site at ease",
-  "url": "http://localhost:1234",
+  "description": "Open Source CMS to build optimized one page site at ease",
+  "url": "http://localhost:8080",
   "theme": "default",
   "version": "v1.0.0"
 }
