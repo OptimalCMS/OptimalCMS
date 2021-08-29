e16f46eb6d17486350ad6de6f6102653f4510cb8
OptimalCMS <85036607+OptimalCMS@users.noreply.github.com>
2021-08-29 21:34:55 +0100

    Update core.php

--- site/core.php
+++ site/core.php
@@ -1,76 +1,34 @@
 <?php
 if ( ! defined('OptimalCMS')) {
-die('OptimalCMS');
+  die('OptimalCMS');
 }
 
 class OptimalCMS {
-    private $hooks;
+  private $hooks;
 
-    function __construct()
-    {
-        $this->hooks = array();
-    }
+  function __construct() {
+    $this->hooks = array();
+  }
 
-    function add_action($where,$callback,$priority=50)
-    {
-        if(!isset($this->hooks[$where]))
-        {
-            $this->hooks[$where] = array();
-        }
-        $this->hooks[$where][$callback]=$priority;
+  function add_action($where,$callback,$priority=50) {
+    if(!isset($this->hooks[$where])) {
+      $this->hooks[$where] = array();
     }
+    $this->hooks[$where][$callback] = $priority;
+  }
 
-    function remove_action($where,$callback)
-    {
-        if(isset($this->hooks[$where][$callback]))
-            unset($this->hooks[$where][$callback]);
-    }
+  function remove_action($where,$callback) {
+    if(isset($this->hooks[$where][$callback]))
+      unset($this->hooks[$where][$callback]);
+  }
 
-    function execute($where,$args=array())
-    {
-        if(isset($this->hooks[$where])&&is_array($this->hooks[$where]))
-        {
-            arsort($this->hooks[$where]);
-            foreach($this->hooks[$where] as $callback=>$priority)
-            {
-                call_user_func_array($callback,$args);
-            }
-        }
+  function execute($where,$args=array()) {
+    if(isset($this->hooks[$where])&&is_array($this->hooks[$where])) {
+      arsort($this->hooks[$where]);
+      foreach($this->hooks[$where] as $callback=>$priority) {
+        call_user_func_array($callback,$args);
+      }
     }
+  }
 };
-
-$OptimalCMS = new OptimalCMS();
-
-function add_action($where,$callback,$priority=50)
-{
-    global $OptimalCMS;
-    if(isset($OptimalCMS))
-        $OptimalCMS->add_action($where,$callback,$priority);
-}
-
-function remove_action($where,$callback)
-{
-    global $OptimalCMS;
-    if(isset($OptimalCMS))
-    $OptimalCMS->remove_action($where,$callback);
-}
-
-function execute_action($where,$args=array())
-{
-    global $OptimalCMS;
-    if(isset($OptimalCMS))
-    $OptimalCMS->execute($where,$args);
-}
-
-
-
-require SITE_DIR . 'db.php';
-
-require THEME_DIR . $site->theme . '/index.php';
-if(file_exists(THEME_DIR . $site->theme . '/functions.php')) {
-require_once(THEME_DIR . $site->theme . '/functions.php');
-}
-
-//require 'theme.php';
-
 ?>
