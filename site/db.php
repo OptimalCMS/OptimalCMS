21a48a26eb7961028f2c4acf4bb0ad397774f82e
OptimalCMS <85036607+OptimalCMS@users.noreply.github.com>
2021-08-29 21:49:52 +0100

    Update db.php

--- site/db.php
+++ site/db.php
@@ -1,21 +1,9 @@
 <?php
-
-if(file_exists('site/site.js')) {
-$db = file_get_contents('site/site.js');
-$site = json_decode($db);
+if(file_exists(SITE_DIR . 'site.js')) {
+  $site = json_decode(file_get_contents(SITE_DIR . 'site.js'));
 }
 
-if(file_exists('site/widgets.js')) {
-$contents = file_get_contents('site/widgets.js');
-$widgets = json_decode($contents);
+if(file_exists(SITE_DIR . 'widgets.js')) {
+  $widgets = json_decode(file_get_contents(SITE_DIR . 'widgets.js'));
 }
-
-if(file_exists('site/posts.js')) {
-$articles = file_get_contents('site/posts.js');
-$posts = json_decode($articles);
-}
-
-if(file_exists('site/pages.js')) {
-$statics = file_get_contents('site/pages.js');
-$pages = json_decode($statics);
-}
+?>
