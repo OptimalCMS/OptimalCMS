fd96c6e5490c52077937e2a7d62109b9e2d30390
OptimalCMS <85036607+OptimalCMS@users.noreply.github.com>
2021-08-30 11:59:47 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 /**
  * @package OptimalCMS
  * @author Shoaiyb Sysa
- * @see https://optimalcms.github.io
+ * @see https://cms.sysa.ml
  * @license MIT
 */
 
@@ -12,10 +12,10 @@
 define( 'OptimalCMS', TRUE );
 define( 'SITE_DIR', __DIR__ . '/site/' );
 define( 'THEME_DIR', __DIR__ . '/themes/' );
+
 /**
  * @require Core
  * @require Functions
 */
-
 require SITE_DIR . 'core.php';
 require SITE_DIR . 'functions.php';
