7f2fc832bd6d16a8b8b7a7cd9c8c357c8d34adca
OptimalCMS <85036607+OptimalCMS@users.noreply.github.com>
2021-09-02 09:53:05 +0100

    Update index.php

--- themes/default/index.php
+++ themes/default/index.php
@@ -3,7 +3,7 @@ if(!defined('OptimalCMS')) {
   die('OptimalCMS');
 }
 
-if(isset($_GET['amp'])) {
+if(isset($_GET['amp']) && file_exists('amp.php')) {
   require 'amp.php';
 } else {
   require 'theme.php';
