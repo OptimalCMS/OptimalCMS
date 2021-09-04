55e14673905f9ec900442e39f238941ec65dab34
OptimalCMS <85036607+OptimalCMS@users.noreply.github.com>
2021-09-04 01:48:57 +0100

    Update site.js

--- site/site.js
+++ site/site.js
@@ -5,7 +5,7 @@
   "subtitle": "Open Source CMS to build optimized onepage site at ease",
   "keywords": "Keywords, Are, Good, For, SEO",
   "description": "Create your own One Page Website in seconds",
-  "url": "http://localhost:1234",
+  "url": "http://localhost:8080",
   "theme": "default",
-  "version": "v1.0.0"
+  "version": "v1.0"
 }
